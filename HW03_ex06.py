e1d1a8e23fb885339e78b02961ca212027b99ea6
danielsgriffin <danielsgriffin@gmail.com>
2015-08-06 11:57:29 -0700

    print fruit

--- HW03_ex06.py
+++ HW03_ex06.py
@@ -60,28 +60,28 @@ def main():
     ############################################################################
     # Uncomment the below to test and before commiting:
     # # Exercise 1
-    # compare(1,1)
-    # compare(1,2)
-    # compare(2,1)
+    # print compare(1,1)
+    # print compare(1,2)
+    # print compare(2,1)
     # # Exercise 2
-    # hypotenuse(1,1)
-    # hypotenuse(3,4)
-    # hypotenuse(1.2,12)
+    # print hypotenuse(1,1)
+    # print hypotenuse(3,4)
+    # print hypotenuse(1.2,12)
     # # Exercise 3
-    # is_between(1,2,3)
-    # is_between(2,1,3)
-    # is_between(3,1,2)
-    # is_between(1,1,2)
+    # print is_between(1,2,3)
+    # print is_between(2,1,3)
+    # print is_between(3,1,2)
+    # print is_between(1,1,2)
     # # Exercise 6
-    # is_palindrome("Python")
-    # is_palindrome("evitative")
-    # is_palindrome("sememes")
-    # is_palindrome("oooooooooooo")
+    # print is_palindrome("Python")
+    # print is_palindrome("evitative")
+    # print is_palindrome("sememes")
+    # print is_palindrome("oooooooooooo")
     # # Exercise 7
-    # is_power(28,3)
-    # is_power(27,3)
-    # is_power(248832,12)
-    # is_power(248844,12)
+    # print is_power(28,3)
+    # print is_power(27,3)
+    # print is_power(248832,12)
+    # print is_power(248844,12)
 
 
 if __name__ == "__main__":
